9a1631f65df1302760fd7ec2ea93b237a1fcd078
wootaiklee <fined000@naver.com>
2024-06-22 18:58:19 +0900

    ajax 리팩토링

--- app.js
+++ app.js
@@ -4,41 +4,37 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+  return JSON.parse(ajax.response);
+}
 
-let newsFeed = JSON.parse(ajax.responseText);
-
-// document.getElementById('root').innerHTML = `
-//   <ul>
-//     ${newsFeed.map((news, index) => `<li key=${index}>${news.title} (${news.url})</li>`).join('')}
-//   </ul>
-// `;
-
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function() {
   const id = location.hash.substr(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-  const newsContents = JSON.parse(ajax.response);
+
+  const newsContents = getData(CONTENT_URL.replace('@id', id));
   const title = this.document.createElement('h1');
-  if (content.innerHTML !== '') {
-    content.innerHTML = '';
-  }
+
   title.innerHTML = newsContents.title;
   content.appendChild(title);
 });
 
 for (let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  const div = document.createElement('div');
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `
+  ul.appendChild(div.firstElementChild)
 }
 
 container.appendChild(ul);
