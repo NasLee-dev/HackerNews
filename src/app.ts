4d9c07d4d6e3e6dcb51911fc4bf67e0901225e6b
wootaiklee <fined000@naver.com>
2024-07-07 16:29:42 +0900

    코드 스플릿팅 + ajax callback

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import Store from './store';
+import { NewsFeedView, NewsDetailView } from './page/index';
+import Router from './core/router';
+
+const store = new Store();
+const router: Router = new Router();
+const newsFeedView = new NewsFeedView('root', store);
+const newsDetailView = new NewsDetailView('root', store);
+
+router.setDefaultPage(newsFeedView);
+
+router.addRoutePath('/page/', newsFeedView);
+router.addRoutePath('/show/', newsDetailView);
+
+router.route();
